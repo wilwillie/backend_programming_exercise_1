49acb897d58759dfae326762da7c0c6bf28b451f
wilwillie <wensteinwilliam@gmail.com>
2024-03-07 14:43:31 +0700

    535230099_WILLIAM_WENSTEIN

---
+++ soal_2.js
@@ -0,0 +1,26 @@
+const students = [
+	{
+  	id: "535250001",
+    score: 98.0
+  },
+  {
+  	id: "535250001",
+    score: 45.0
+  },
+  {
+  	id: "535250001",
+    score: 97.75
+  },
+  {
+  	id: "535250001",
+    score: 99.25
+  },
+  {
+  	id: "535250001",
+    score: 78.5
+  },
+];
+
+students.sort((a,b) => b.score - a.score); // FIXME
+
+console.log(students);
